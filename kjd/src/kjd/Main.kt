b523df99802d6ea6c6bcccd39ba875a4ac30a6d3
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-08-24 15:04:21 -0300

    First hello world, mutable and immutable variables and type inference.

--- kjd/src/kjd/Main.kt
+++ kjd/src/kjd/Main.kt
@@ -1,5 +1,9 @@
 package kjd
 
 fun main(args:Array<String>){
+    println("Hello World")
 
+    var x: Int = 10 //mutable
+    val y: Int = 10 //like final in Java, immutable
+    var z = 10 //type inference. Kotlin is statically typed, It's not dynamic typed as Javascript
 }
